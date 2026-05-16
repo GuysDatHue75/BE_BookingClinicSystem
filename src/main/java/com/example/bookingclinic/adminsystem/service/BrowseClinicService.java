34447cc06475fde92fb2c7fc4ee718412207d56c
Tran Huu Minh Quan <minhquantranhuu2004@gmail.com>
2026-05-16 21:54:10 +0700

    fix

--- src/main/java/com/example/bookingclinic/adminsystem/service/BrowseClinicService.java
+++ src/main/java/com/example/bookingclinic/adminsystem/service/BrowseClinicService.java
@@ -1,7 +1,5 @@
 package com.example.bookingclinic.adminsystem.service;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 
 import com.example.bookingclinic.adminsystem.dto.request.BrowseClinicActionRequest;
